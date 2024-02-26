476bde63a9f233f9d79c6be679e04ab57e74af45
Iliyas <ilyas.baygozy@mail.ru>
2024-02-27 01:07:03 +0600

    fixai

--- api/config.py
+++ api/config.py
@@ -18,25 +18,28 @@ AUCH_ENABLE = os.getenv("AUCH_ENABLE", "0")
 
 """ read https://ai.google.dev/api/rest/v1/GenerationConfig """
 generation_config = {
-    "max_output_tokens": 1024,
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
 }
 
 """ read https://ai.google.dev/api/rest/v1/HarmCategory """
 safety_settings = [
     {
         "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE"
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
     },
     {
         "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE"
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
     },
     {
         "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE"
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
     },
     {
         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE"
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
     },
 ]
